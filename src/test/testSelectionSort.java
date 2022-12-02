309d6a5cae4fbd8d8dd4ecfe36dcd9a7102fd463
Abeer Waza <awaza@umich.edu>
2022-12-02 05:16:19 -0500

    PositivesandNegatives branch created and modified

--- src/test/testSelectionSort.java
+++ src/test/testSelectionSort.java
@@ -28,11 +28,33 @@ public class testSelectionSort {
         Sortedarr[4] = 10;
 
         /** add tests to check for this unit test **/
+        SelectionSort x = new SelectionSort();
+        arr = x.basicSelectionSort(arr);
+
+        assertArrayEquals("Positive Test Fails", Sortedarr, arr);
     }
 
     public void testNegative(){
 
+        int[] arr = new int[5];
+        arr[0] = -8;
+        arr[1] = -9;
+        arr[2] = -7;
+        arr[3] = -10;
+        arr[4] = -2;
+
+        int[] Sortedarr = new int[5];
+        Sortedarr[0] = -10;
+        Sortedarr[1] = -9;
+        Sortedarr[2] = -8;
+        Sortedarr[3] = -7;
+        Sortedarr[4] = -2;
+
         /** Test data contains negative values only **/
+        SelectionSort x = new SelectionSort();
+        arr = x.basicSelectionSort(arr);
+
+        assertArrayEquals("Negative Test Fails", Sortedarr, arr);
     }
 
     public void testMixed(){
